37ef063a2243d12bf77cf0d4fa27dac83f587c8d
Peter & Erin <student@epicodus.com>
2015-04-22 09:29:26 -0700

    first spec test failed

---
+++ spec/title_case_spec.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('title_case')
+require('pry')
+
+describe('String#title_case') do
+
+    it('capitalizes first letter in a title string') do
+      expect("beowolf".title_case().to(eq("Beowulf")))
+
+    end
+
+
+end
