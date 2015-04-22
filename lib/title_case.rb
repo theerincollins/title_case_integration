489c09460dea08bfc26aeba9f94ff8014c4196c7
Peter & Erin <student@epicodus.com>
2015-04-22 09:56:22 -0700

    capitalizes first letter of all words in a title string

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,6 +1,17 @@
 class String
   define_method(:title_case) do
-    self.capitalize()
+
+    title_string = self.split(" ")
+    final_string = []
+
+    title_string.each do |word|
+
+      final_string.push(word.capitalize())
+    end
+
+  final_string.join(" ")
+
+    #self.capitalize()
 
 end
 end
